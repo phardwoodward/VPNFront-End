96a48ae1652ae622613c6f5c674607b8f45da9a8
phardwoodward <65140787+phardwoodward@users.noreply.github.com>
2022-03-23 14:18:02 -0400

    initial github front-end repo

---
+++ app/src/main/java/com/example/vpn_frontend_paul/vpn_on.kt
@@ -0,0 +1,11 @@
+package com.example.vpn_frontend_paul
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class vpn_on : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vpn_on)
+    }
+}
